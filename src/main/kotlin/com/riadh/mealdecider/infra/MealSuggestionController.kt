76f1d1c99fed93a3d7635afafc7d61c1f06c32d9
Riadh MNASRI <riadh.mnasri@gmail.com>
2019-01-01 19:52:05 +0100

    Expose API

--- src/main/kotlin/com/riadh/mealdecider/infra/MealSuggestionController.kt
+++ src/main/kotlin/com/riadh/mealdecider/infra/MealSuggestionController.kt
@@ -8,9 +8,14 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toFlux
 import reactor.core.publisher.toMono
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PostMapping
 
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 class MealSuggestionController(val mealSuggestionService: MealSuggestionService){
 
     @GetMapping("/meals")
@@ -24,5 +29,22 @@ class MealSuggestionController(val mealSuggestionService: MealSuggestionService)
         return foundSuggestion.toMono()
     }
 
+    @GetMapping("/mealSuggestion/{id}")
+    fun getMealSuggestionbyId(@PathVariable("id") id: Int): Mono<ResponseEntity<MealSuggestion>> {
+        return mealSuggestionService.getMealById(id)
+            .map { ResponseEntity.ok(it) }
+            .orElse(ResponseEntity(HttpStatus.NOT_FOUND))
+            .toMono()
+    }
+
+    @PostMapping("/mealSuggestion/save")
+    fun saveMealSuggestion(@RequestBody mealSuggestion: MealSuggestion): Mono<MealSuggestion> {
+        return mealSuggestionService.saveMeal(mealSuggestion).toMono()
+    }
+
+    @PostMapping("/mealSuggestion/delete/{id}")
+    fun deleteMealSuggestion(@PathVariable("id") id: Int){
+        return mealSuggestionService.deleteMealById(id)
+    }
 
 }
--- src/main/kotlin/com/riadh/mealdecider/infra/MealSuggestionService.kt
+++ src/main/kotlin/com/riadh/mealdecider/infra/MealSuggestionService.kt
@@ -3,6 +3,7 @@ package com.riadh.mealdecider.infra
 import com.riadh.mealdecider.domain.MealSuggestion
 import com.riadh.mealdecider.tools.random
 import org.springframework.stereotype.Service
+import java.util.Optional
 
 @Service
 class MealSuggestionService(val mealSuggestionRepository: MealSuggestionRepository) {
@@ -13,5 +14,17 @@ class MealSuggestionService(val mealSuggestionRepository: MealSuggestionReposito
 
     fun getAllMeals(): List<MealSuggestion>{
         return mealSuggestionRepository.findAll().toList()
+    }
+
+    fun getMealById(id: Int): Optional<MealSuggestion> {
+        return mealSuggestionRepository.findById(id)
+    }
+
+    fun saveMeal(mealSuggestion: MealSuggestion): MealSuggestion{
+        return mealSuggestionRepository.save(mealSuggestion)
+    }
+
+    fun deleteMealById(id: Int){
+        return mealSuggestionRepository.deleteById(id)
     }
 }
