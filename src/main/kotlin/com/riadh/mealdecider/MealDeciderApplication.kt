2561b62c8652e2b5fd137a86485f4051612b0e27
Riadh MNASRI <riadh.mnasri@gmail.com>
2019-01-01 23:38:07 +0100

    Minor Refactoring

--- src/main/kotlin/com/riadh/mealdecider/MealDeciderApplication.kt
+++ src/main/kotlin/com/riadh/mealdecider/MealDeciderApplication.kt
@@ -1,4 +1,4 @@
-package com.riadh
+package com.riadh.mealdecider
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/riadh/mealdecider/MealDeciderApplicationTests.kt
+++ src/test/kotlin/com/riadh/mealdecider/MealDeciderApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.riadh
+package com.riadh.mealdecider
 
 import org.junit.Test
 import org.junit.runner.RunWith
